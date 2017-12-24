396bdee1ff32c8d8335ef31b45c45551f2f63d85
tayabsoomro <isoomro.t@gmail.com>
2017-12-24 14:59:49 -0600

    add basic titration curve

--- main/js/titrationCurve.js
+++ main/js/titrationCurve.js
@@ -11,7 +11,7 @@ var y = d3.scaleLinear().range([height, 0]);
 
 // Scale the range of the data
 x.domain([0,2]);
-y.domain([0,20]);
+y.domain([0,14]);
 
 // append the svg object to the body of the page
 // appends a 'group' element to 'svg'
@@ -23,3 +23,48 @@ var svg = d3.select(".titrationRender").append("svg")
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json('./assets/data/diproticGraphData.json', function(error,data){
+    if(error) throw error;
+    console.log(data);
+
+    data.forEach(function(d){
+           d.y_pH = +d.y_pH;
+           d.x_OH = +d.x_OH;
+    });
+
+    var vline = d3.line()
+        .curve(d3.curveLinear)
+        .x(function(d){ return x(d.x_OH); })
+        .y(function(d){ return y(d.y_pH); });
+
+
+    xAxis = d3.axisBottom(x).tickValues([]);
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add x Axis text
+    svg.append("text")
+        .text("OH added (equivalents) ")
+        .attr("transform", "translate(" +  width/2 + "," + (height + 40) + ")");
+
+
+    yAxis = d3.axisLeft(y).tickValues([]);
+
+    // Add the Y Axis
+    svg.append("g")
+        .call(yAxis);
+
+    // Add y Axis text
+    svg.append("text")
+        .text("pH")
+        .attr("transform", "translate(-40," + (height /2) + ") rotate(-90)");
+
+
+    path = svg.append("path")
+        .attr("d", vline(data))
+        .attr("class", "line");
+});
